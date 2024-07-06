eadb4917d00a02a18ad4be74208cf9d0e9be4d9e
jcruzg <jcruzg@ucenfotec.ac.cr>
2024-07-06 07:07:32 -0600

    se versiona el application.properties

--- src/main/java/com/project/demo/rest/auth/AuthRestController.java
+++ src/main/java/com/project/demo/rest/auth/AuthRestController.java
@@ -12,9 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Optional;
 
 @RequestMapping("/auth")
@@ -75,4 +77,5 @@ public class AuthRestController {
         User savedUser = userRepository.save(user);
         return ResponseEntity.ok(savedUser);
     }
+
 }
